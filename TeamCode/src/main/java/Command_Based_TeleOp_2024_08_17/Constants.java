e638fc4e784bc3929dcb46c797b57ee6757b7c64
CorpuzIhate <guyihate@gmail.com>
2024-10-25 14:48:09 -0400

    -tuned p-Constant and setpoints to bore encoder on the shoulder

--- TeamCode/src/main/java/Command_Based_TeleOp_2024_08_17/Constants.java
+++ TeamCode/src/main/java/Command_Based_TeleOp_2024_08_17/Constants.java
@@ -5,16 +5,16 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public final class Constants{
     public static final class ShoulderPIDConstants{
-        public static final double kP = 0.05;
+        public static final double kP = 0.001;
         public static final double kI = 0;
         public static final double kD = 0;
         public static final double kF = 0;
 
     }
     public static final class ShoulderSetpoints{
-        public static final double middleArmPos = 1000;
+        public static final double middleArmPos = 1200;
 
-        public static final double upperArmPos = 2000;
+        public static final double upperArmPos = 1700;
     };
 
 

a2096b9ec419e4bbec846d7056e2a818a95aad12
CorpuzIhate <guyihate@gmail.com>
2024-11-19 10:16:29 -0500

    (functional) got rid of I and D terms on arm joint Pid Controller

--- TeamCode/src/main/java/Arm_A_Kraken_DocBotics_FTC_2024/Constants.java
+++ TeamCode/src/main/java/Arm_A_Kraken_DocBotics_FTC_2024/Constants.java
@@ -6,11 +6,12 @@ import com.acmerobotics.dashboard.config.Config;
 public final class Constants{
     public static final class ShoulderPIDConstants{
         public static final double kP = 0.001;
-        public static final double kI = 0.000001;
-        public static final double kD = 0.00005;
+        public static final double kI = 0;
+        public static final double kD = 0;
         public static final double kF = 0;
 
     }
+    //test
     public static final class ElbowPIDConstants{
         public static final double kP = 0.001;
         public static final double kI = 0;

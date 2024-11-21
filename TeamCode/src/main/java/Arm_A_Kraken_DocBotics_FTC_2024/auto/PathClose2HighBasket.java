20fc3b7f517905de70c18540e70db93118212637
CorpuzIhate <guyihate@gmail.com>
2024-11-21 14:39:21 -0500

    - added path for PathFarObservePark

---
+++ TeamCode/src/main/java/Arm_A_Kraken_DocBotics_FTC_2024/auto/PathClose2HighBasket.java
@@ -0,0 +1,4 @@
+package Arm_A_Kraken_DocBotics_FTC_2024.auto;
+
+public class PathClose2HighBasket extends AutoRobotContainer{
+}
---
+++ TeamCode/src/main/java/Arm_A_Kraken_DocBotics_FTC_2024/auto/PathFarDropObservePark.java
@@ -0,0 +1,46 @@
+package Arm_A_Kraken_DocBotics_FTC_2024.auto;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import Arm_A_Kraken_DocBotics_FTC_2024.AutoCommands.MoveRobotEncoderXYCMD;
+import Arm_A_Kraken_DocBotics_FTC_2024.Constants;
+
+@Autonomous
+public class PathFarDropObservePark extends AutoRobotContainer {
+    @Override
+    public void path(){
+        schedule(new SequentialCommandGroup(
+                        new MoveRobotEncoderXYCMD(24,-24,3, 0.5,
+                        mecanumDriveBaseSub, telemetryManagerSub.getTelemetryObject()),
+                    new MoveRobotEncoderXYCMD(24,24,3, 0.5,
+                        mecanumDriveBaseSub, telemetryManagerSub.getTelemetryObject()),
+
+                        new InstantCommand(() -> {
+                            shoulderSub.setSetpoint(Constants.ShoulderSetpoints.shoulderClearancePos);
+                            elbowSub.setSetpoint(Constants.ElbowSetpoints.elbowClearancePos);
+                        } ),
+                        new WaitCommand(3000),
+                        new InstantCommand(() -> {
+                            shoulderSub.setSetpoint(Constants.ShoulderSetpoints.shoulderSubmersiblePickUpPos);
+                            elbowSub.setSetpoint(Constants.ElbowSetpoints.elbowSubmersiblePickUpPos);
+                        }),
+                        new WaitCommand(3000),
+                        new InstantCommand(() -> {
+                            shoulderSub.setSetpoint(300);
+                            elbowSub.setSetpoint(100);
+                        })
+
+
+
+
+
+                )
+
+
+        );
+    }
+}
+
